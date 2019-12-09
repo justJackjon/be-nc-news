68ed62c26b6881998ac1a6c3e1653e7fdec2c23d
justJackjon <github@redrobincreative.com>
2019-12-09 13:03:32 +0000

    Update to package.json seed scripts to distinguish between environments

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -11,8 +11,18 @@ exports.seed = function(knex) {
   const topicsInsertions = knex('topics').insert(topicData);
   const usersInsertions = knex('users').insert(userData);
 
-  return Promise.all([topicsInsertions, usersInsertions])
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
     .then(() => {
+      return Promise.all([topicsInsertions, usersInsertions]);
+    })
+    .then(() => {
+      const formattedData = formatDates(articleData);
+      return knex('articles')
+        .insert(formattedData)
+        .returning('*');
+
       /* 
       
       Your article data is currently in the incorrect format and will violate your SQL schema. 
@@ -23,6 +33,7 @@ exports.seed = function(knex) {
       */
     })
     .then(articleRows => {
+      console.log(articleRows);
       /* 
 
       Your comment data is currently in the incorrect format and will violate your SQL schema. 
