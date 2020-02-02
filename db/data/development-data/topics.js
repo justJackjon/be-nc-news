f710c2fa6158aec8c4420119f69d73a61e0af5a5
justJackjon <github@redrobincreative.com>
2020-02-02 20:01:40 +0000

    update heroku settings

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -2,4 +2,6 @@ module.exports = [
   { description: 'Code is love, code is life', slug: 'coding' },
   { description: 'FOOTIE!', slug: 'football' },
   { description: 'Hey good looking, what you got cooking?', slug: 'cooking' },
+  { description: 'Let’s explore.', slug: 'National Geographic' },
+  { description: 'Explore the universe with NASA.', slug: 'NASA' }
 ];
